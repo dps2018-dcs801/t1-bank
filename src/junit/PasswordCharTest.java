a71a9b629a3a1e40b22b7b7df8302ed6a6ed6ab2
Tom Lahoud <tomlahoud@comcast.net>
2015-10-18 15:05:50 -0400

    updated PasswordCharTest

--- src/junit/PasswordCharTest.java
+++ src/junit/PasswordCharTest.java
@@ -14,10 +14,28 @@ public class PasswordCharTest {
 		Assert.assertEquals("Password must contain at least 1 non-alphanumeric character", PasswordManager.validate(password));
 	}
 	
+	@Test
+	public void checkValidPassword() {
+		String password = "J12*";
+		Assert.assertEquals("valid", PasswordManager.validate(password));
+	}
 	
 	@Test
 	public void checkAtLeastOneANumeric() {
 		String password = "$#$";
 		Assert.assertEquals("Password must contain at least 1 alphanumeric character", PasswordManager.validate(password));
 	}
+	
+	@Test
+	public void checkOneSpace() {
+		String password = " $";
+		Assert.assertEquals("Password cannot contain space(s)", PasswordManager.validate(password));
+	}
+	
+	@Test
+	public void checkSpaceInPassword() {
+		String password = "1 $";
+		Assert.assertEquals("Password cannot contain space(s)", PasswordManager.validate(password));
+	}
+
 }
